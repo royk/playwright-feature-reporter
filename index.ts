955b6d6a24418661bd5ea6a58a9c00247b5b3518
Roy Klein <roeiklein@gmail.com>
2024-09-14 19:53:32 +0200

    feat: initial commit

---
+++ index.ts
@@ -0,0 +1,76 @@
+import type {
+    FullConfig, FullResult, Reporter, Suite, TestCase, TestResult
+  } from '@playwright/test/reporter';
+  import fs from 'fs';
+  let _suite: Suite;
+  let _outputFile: string;
+  class MyReporter implements Reporter {
+    constructor(options: { outputFile?: string } = {}) {
+      _outputFile = options.outputFile || 'FEATURES.md';
+    }
+    onBegin(config: FullConfig, suite: Suite) {
+      _suite = suite;
+    }
+  
+    onTestBegin(test: TestCase, result: TestResult) {
+    }
+  
+    onTestEnd(test: TestCase, result: TestResult) {
+    }
+    
+    
+      
+    onEnd(result: FullResult) {
+  
+      function getOutcome(testCase: TestCase) {
+        switch (testCase.outcome()) {
+          case 'skipped':
+            return ':construction:';
+          case 'expected':
+            return ':white_check_mark:';
+          case 'unexpected':
+            return ':x:';
+          case 'flaky':
+            return ':warning:';
+        }
+        return testCase.outcome();
+      }
+      let nestedLevel = 0;
+      let projectCount = 0;
+      let stringBuilder = '';
+      function printSuite(s: Suite) {
+        const consolePrefix = '\t'.repeat(nestedLevel);
+        const mdHeaderPrefix = '  '.repeat(nestedLevel) + '#'.repeat(nestedLevel+2);
+        const mdListPrefix = '  '.repeat(nestedLevel) + '-';
+        if (s.type === 'project') {
+          projectCount++;
+        }
+        if (projectCount > 1) {
+          return;
+        }
+        if (s.type === 'describe') {
+          console.log(`${consolePrefix} Feature: ${s.title}`);
+          if (nestedLevel===0) {
+            stringBuilder += `${mdHeaderPrefix} Feature: ${s.title}\n`;
+          } else {
+            stringBuilder += `${mdHeaderPrefix} ${s.title}\n`;
+          }
+          nestedLevel++;
+        }
+        s.tests.forEach((test) => {
+          console.log(`${consolePrefix}\t${test.title}`);
+          stringBuilder += `${mdListPrefix} ${getOutcome(test)} ${test.title}\n`;
+        });
+        s.suites.forEach((ss) => {
+          printSuite(ss);
+        });
+        if (s.type === 'describe') {
+          nestedLevel--;
+        }
+      }
+      printSuite(_suite);
+      fs.writeFileSync(_outputFile, stringBuilder);
+    }
+  }
+  
+  export default MyReporter;
