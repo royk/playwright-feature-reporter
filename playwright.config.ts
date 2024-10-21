4c74c925835cd2172559213b37caadf99d8ae273
Roy Klein <roeiklein@gmail.com>
2024-10-21 08:01:44 +0200

    chore: when running tests on watch, don't generate reports [skip ci]

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig, devices } from '@playwright/test';
 
+const isWatch = process.env.PWTEST_WATCH === '1';
+const localReporter = isWatch ? 'list' : [['list'],['html'],['./dist/index.js', { outputFile: './README.md', fullReportLink: 'playwright-report/index.html' }]];
 export default defineConfig({
   testDir: './src',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? [['github'],['html'],['./dist/index.js', { outputFile: './README.md', fullReportLink: 'https://raw.githack.com/royk/playwright-feature-reporter/refs/heads/main/playwright-report/index.html' }]] : [['list'],['html'],['./dist/index.js', { outputFile: './README.md', fullReportLink: 'playwright-report/index.html' }]],
+  // @ts-ignore
+  reporter: process.env.CI ? [['github'],
+                              ['html'],
+                              ['./dist/index.js', { outputFile: './README.md', fullReportLink: 'https://raw.githack.com/royk/playwright-feature-reporter/refs/heads/main/playwright-report/index.html' }]] 
+                              : localReporter,
   use: {
     trace: 'on-first-retry',
   },
